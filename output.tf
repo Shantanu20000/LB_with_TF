4fbd9172e29f07bff75aee8c2e65b2ec27f22e0c
Shantanu Vanarase <163661534+Shantanu20000@users.noreply.github.com>
2024-06-09 23:25:27 +0530

    Delete output.tf

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "instance_ids" {
-  description = "The IDs of the created EC2 instances"
-  value       = module.my_instances[*].instance_ids
-}
-
-output "subnet_id1" {
-  value = module.vpc.public_subnet_1a
-  description="This is my public subnet id"
-}
