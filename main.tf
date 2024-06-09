cc4b346556f6347f087fbd84319b7e6d80883a6c
Shantanu Vanarase <163661534+Shantanu20000@users.noreply.github.com>
2024-06-09 23:34:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,9 +10,6 @@ terraform {
 
 provider "aws" {
     region = "us-east-1"
-   # shared_config_files =["/root/.aws/config"]  
-   # shared_credentials_files = ["/root/.aws/credentials"]
-    # profile = "balu"
 }
 
 # Use the EC2 instances module
